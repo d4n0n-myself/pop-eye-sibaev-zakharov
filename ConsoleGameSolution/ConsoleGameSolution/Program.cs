fdc43a4a987bb80f7e2ee72fe499aeea9cc5c62b
d4n0n_myself <danon.sibaev@gmail.com>
2017-12-01 21:47:40 +0300

    d4n0n: Added ghosts, added gameOver rule, added side walls(experimental), CONTROLS TO @ : WASD

--- ConsoleGameSolution/ConsoleGameSolution/Program.cs
+++ ConsoleGameSolution/ConsoleGameSolution/Program.cs
@@ -1,118 +1,148 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Threading;
 
-namespace ConsoleGameSolution
+namespace ConsoleGame
 {
     /// <summary>
-    /// Made by Danel Sibaev 11-706 and Kirill Zakharov 11-707
+    /// Made by Danel Sibaev 11-706 and Zakharov Kirill 11-707
     /// </summary>
-    internal class Program
+    class Program
     {
-        public static void Main(string[] args)
+        public class At
         {
-            Console.BufferWidth = Console.WindowWidth = 32;
-            Console.BufferHeight = Console.WindowHeight = 22;
+            private readonly int _xLimit;
+            private readonly int _yLimit;
+            public At()
+                : this(Console.WindowWidth / 2, 0, Console.WindowWidth - 1, Console.WindowHeight - 2)
+            { }
+
+            public At(int x, int y, int xLimit, int yLimit)
+            {
+                X = 1;
+                Y = 1;
+                _xLimit = xLimit;
+                _yLimit = yLimit;
+                // Производим первичную отрисовку
+                Console.SetCursorPosition(X, Y);
+                Console.Write('@');
+            }
+            public int X { get; private set; }
+            public int Y { get; private set; }
+
+            public void Move(ConsoleKey direction)
+            {
+                // Стираем следы (все пиксели игрового объекта на поле)
+                Console.SetCursorPosition(X, Y);
+                Console.Write(' ');
+                switch (direction)
+                {
+                    case ConsoleKey.A:
+                        if (X > 1) X--;
+                        break;
+                    case ConsoleKey.D:
+                        if (X < _xLimit - 2) X++;
+                        break;
+                    case ConsoleKey.W:
+                        if (Y > 1) Y--;
+                        break;
+                    case ConsoleKey.S:
+                        if (Y < _yLimit - 1)
+                            Y++;
+                        break;
+                }
+                // Отрисовываем объект в новой позиции
+                Console.SetCursorPosition(X, Y);
+                Console.Write('@');
+            }
+        }
+
+        public class Ghost
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
+        public static List<Ghost> CreateGhosts()
+        {
+            var ghosts = new List<Ghost>();
+            var random = new Random();
+            //d4n0n - попытка обработать гостов
+            
+            for (int i=0;i<3;i++)
+            {
+                ghosts.Add(new Ghost { X = random.Next(1, 89), Y = random.Next(1, 29) });
+                Console.SetCursorPosition(ghosts[i].X, ghosts[i].Y);
+                Console.Write('2');
+            }
+
+            return ghosts;
+        }
+
+        static void Main()
+        {
+            // Инициализация параметров
+            Console.BufferWidth = Console.WindowWidth = 91;
+            Console.BufferHeight = Console.WindowHeight = 31;
+            // Прячем курсор для красоты
             Console.CursorVisible = false;
             // Установка параметра задержки смены кадров
             const int frameDelay = 100;
+            var stopwatch = new Stopwatch();
+
+            //d4n0n - Создать стены(пример обводки)
+            for(int i=0;i<30;i++)
+                for (int j=0;j<90;j++)
+                    if (i == 0 || j == 0 || i == 29 || j == 89)
+                    {
+                        Console.SetCursorPosition(j, i);
+                        Console.Write('+');
+                    }
+
+            //d4n0n - создать ghost'ов
+            var ghosts = CreateGhosts();
             
+            // Создаём игровые объекты
+            var at = new At();
+
             bool gameOver = false;
-            
+            //Игровой цикл
             while (!gameOver)
             {
-                // d4n0n : 
-                // прописать прорисовку стен (рандомную)
-                // через массивы?
-                // будет необходимо условие чтобы 1 строка была со стенами, а другая нет (поочередно)
-                // идея : заполнение поочередно полных строк стен, затем вырезать проходы
-                /*
-                пример - 
-                
-               __________________
-               |§              #|
-               |++  ++++++++++++|                
-               |               §|
-               |+++++++++++  +++|
-               |@               |
-               ------------------                 
-               
-               @ - собачка, герой, мы им управляем
-               § - ghost ( может быть другой символ)
-               # - место, куда надо прийти
-               
-               lvl должен быть больше (около трех дырок в стене)
-                 */
-                
-                
-                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+                // В нём нужно выполнять действия по нажатию кнопок
+                // Обновлять положения объектов
+                // Определять столкновения и всё такое
+
+                // Обработка нажатий клавиатуры
                 if (Console.KeyAvailable)
                 {
                     var keyPressed = Console.ReadKey(true).Key;
                     while (Console.KeyAvailable)
                         Console.ReadKey(true);
                     if (keyPressed == ConsoleKey.Escape) break;
-                        
-                    // Перемещение объектов по нажатию кнопок
-                    ConsoleGame.Move(keyPressed);
-                    
-                    // Вычисление задержки для поддержания стабильного FPS
-                    stopwatch.Stop();
-                    int sleepTime = Math.Max(frameDelay - (int) stopwatch.Elapsed.TotalMilliseconds,0);
-                    // Задержка
-                    Thread.Sleep(sleepTime);
-                }
-                
-                //описать проверки на врезание в стены или в госта
 
+                    // Перемещение объектов по нажатию кнопок
+                    at.Move(keyPressed);
+                }
+
+                for (int i = 0; i < ghosts.Count; i++)
+                    if (at.X == ghosts[i].X && at.Y == ghosts[i].Y)
+                        gameOver = true;
+
+                //НУЖНО РЕАЛИЗОВАТЬ ДВИЖЕНИЕ ГОСТОВ
+
+
+                // Вычисление задержки для поддержания стабильного FPS
+                stopwatch.Stop();
+                int sleepTime = Math.Max(frameDelay - (int)stopwatch.Elapsed.TotalMilliseconds, 0);
+                // Задержка
+                Thread.Sleep(sleepTime);
             }
             Console.Clear();
+            Console.WriteLine("You've lost.");
+            Console.ReadLine();
         }
-    }
-
-    public class Ghost
-    {
-        public static int X { get; private set; }
-        public static int Y { get; private set; }
-    }
-
-    public class ConsoleGame
-    {
-        public static int X { get; private set; }
-        public static int Y { get; private set; }
-        public static int XLimit;
-        public static int YLimit;
-
-        public void CollectLimits(int xLimit, int yLimit)
-        {
-            XLimit = xLimit;
-            YLimit = yLimit;
-        }
-        
-        public static void Move(ConsoleKey dir)
-        {
-            Console.SetCursorPosition(X, Y);
-            Console.Write(' ');
-            switch (dir)
-            {
-                    case ConsoleKey.W:
-                        if (Y > 0) Y--;
-                        break;
-                    case ConsoleKey.A:
-                        if (X > 0) X--;
-                        break;
-                    case ConsoleKey.S:
-                        if (Y < XLimit) Y++;
-                        break;
-                    case ConsoleKey.D:
-                        if (X < YLimit) X++;
-                        break;
-            }
-            Console.SetCursorPosition(X,Y);
-            Console.Write('@');
-        }
-
     }
 }
