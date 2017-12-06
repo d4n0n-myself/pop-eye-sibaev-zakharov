79c771547d33ca4c1371dd98c33ed9155d124f72
d4n0n_myself <danon.sibaev@gmail.com>
2017-12-06 23:05:37 +0300

    d4n0n: damaged ghosts movement, non-ready fields, random ghosts creation

--- ConsoleGameSolution/ConsoleGameSolution/Program.cs
+++ ConsoleGameSolution/ConsoleGameSolution/Program.cs
@@ -11,159 +11,179 @@ namespace ConsoleGameSolution
     /// <summary>
     /// Made by Danel Sibaev 11-706 and Zakharov Kirill 11-707
     /// </summary>
-    
-    
+
+
     class Program
     {
-        public class At
+        public class Field
         {
             public static int XLimit;
             public static int YLimit;
-            
-            public At()
-                : this(Console.WindowWidth - 1, Console.WindowHeight - 2)
+
+            public Field()
+                : this(Console.WindowWidth, Console.WindowHeight)
             { }
 
-            public At(int xLimit, int yLimit)
+            public Field(int xLimit, int yLimit)
             {
                 XLimit = xLimit;
                 YLimit = yLimit;
             }
         }
 
-        public class Entity
+        public class Object
         {
             public int X { get; set; }
             public int Y { get; set; }
         }
-        
-        public class Player : Entity
+
+        public class Player : Object
         {
-            public void Create()
+            public Player Create()
             {
+                Player player = new Player { X = 1, Y = 1 };
                 Console.SetCursorPosition(X, Y);
                 Console.Write('@');
+                return player;
             }
+
             
             public void Move(ConsoleKey direction)
             {
                 // Стираем следы (все пиксели игрового объекта на поле)
                 Console.SetCursorPosition(X, Y);
                 Console.Write(' ');
+
                 switch (direction)
                 {
-                    //для первого игрока
                     case ConsoleKey.A:
                         if (X > 1) X--;
                         break;
                     case ConsoleKey.D:
-                        if (X < At.XLimit - 2) X++;
+                        if (X < Field.XLimit - 2) X++;
                         break;
                     case ConsoleKey.W:
-                        if (Y > 1) Y--;
+                        if (Y > 0 && !Walls.walls[Y, X]) Y--;
                         break;
                     case ConsoleKey.S:
-                        if (Y < At.YLimit - 1)
+                        if (Y < Field.YLimit - 3 && !Walls.walls[Y, X])
                             Y++;
                         break;
-                        
-                    //для второго игрока
-                    case ConsoleKey.UpArrow:
-                        if (Y > 1) Y--;
-                        break;
-                    case ConsoleKey.DownArrow:
-                        if (Y < At.YLimit - 1) Y++;
-                        break;
-                    case ConsoleKey.LeftArrow:
-                        if (X > 1) X--;
-                        break;
-                    case ConsoleKey.RightArrow:
-                        if (X < At.XLimit - 2) X++;
-                        break;
                 }
+
                 // Отрисовываем объект в новой позиции
                 Console.SetCursorPosition(X, Y);
                 Console.Write('@');
             }
-        }
 
-        public class Ghost : Entity
-        {
-        }
-
-        public class DestinationPoint : Entity
-        {
-        }
-
-        public static List<Ghost> CreateGhosts()
-        {
-            var ghosts = new List<Ghost>();
-            var random = new Random();
-            //d4n0n - попытка обработать гостов
-            
-            for (int i=0;i<3;i++)
+            public class Ghost : Object
             {
-                ghosts.Add(new Ghost { X = random.Next(1, 89), Y = random.Next(1, 29) });
-                Console.SetCursorPosition(ghosts[i].X, ghosts[i].Y);
-                Console.Write('2');
-            }
+                public bool DirectedToRightSide;
 
-            return ghosts;
-        }
+                public List<Ghost> CreateGhosts()
+                {
+                    var ghosts = new List<Ghost>();
+                    var random = new Random();
+                    var countOfGhosts = random.Next(3,Field.YLimit / 2 + 1);
+                    //d4n0n - создание ghosts
 
-        static void Main()
-        {
-            // Инициализация параметров
-            Console.BufferWidth = Console.WindowWidth = 91;
-            Console.BufferHeight = Console.WindowHeight = 31;
-            // Прячем курсор для красоты
-            Console.CursorVisible = false;
-            // Установка параметра задержки смены кадров
-            const int frameDelay = 100;
-            var stopwatch = new Stopwatch();
-
-            //d4n0n - Создать стены(пример обводки)
-            for(int i=0;i<30;i++)
-                for (int j=0;j<90;j++)
-                    if (i == 0 || j == 0 || i == 29 || j == 89)
+                    for (int i = 0; i < countOfGhosts; i++)
                     {
-                        Console.SetCursorPosition(j, i);
-                        Console.Write('#');
+                        var xPos = random.Next(1, Field.XLimit - 2);
+                        var yPos = random.Next(Field.YLimit / 2 - 1) * 2;
+                        var randomDirection = random.Next(2);
+
+                        foreach (var ghost in ghosts)
+                            if (ghost.Y == yPos)
+                            {
+                                i--;
+                                break;
+                            }
+                        
+                        ghosts.Add(new Ghost {  X = xPos, Y = yPos, DirectedToRightSide = randomDirection == 1 ? true : false });
+                        Console.SetCursorPosition(ghosts[i].X, ghosts[i].Y);
+                        Console.Write('2');
                     }
 
-            //d4n0n - создать ghost'ов
-            var ghosts = CreateGhosts();
-            
-            // Создаём игровые объекты
-            var at = new At();
+                    return ghosts;
+                }
 
-            
-            //d4n0n - создание player ов
-            Player[] players = new Player[2];
-            for (int i = 0; i < 2; i++)
-                players[i].Create();
-            
-            //d4n0n - создание destination point
-            DestinationPoint point = new DestinationPoint();
-            var random = new Random();
-            point.X = random.Next(At.XLimit) + 1;
-            point.Y = At.YLimit;
-            
-            //d4n0n - подойдет ли вообще реализация двух игроков?
-            
-            bool gameOver = false;
-            //Игровой цикл
-            while (!gameOver)
-            {
-                stopwatch.Start();
-                // В нём нужно выполнять действия по нажатию кнопок
-                // Обновлять положения объектов
-                // Определять столкновения и всё такое
-
-                // Обработка нажатий клавиатуры
-                if (Console.KeyAvailable)
+                public void Move()
                 {
-                    for (int i = 0; i < 2; i++)
+                    Console.SetCursorPosition(X, Y);
+                    Console.Write(' ');
+
+                    if (this.DirectedToRightSide == true && this.X < Field.XLimit - 2)
+                        X++;
+                    if (this.X == Field.XLimit - 3) DirectedToRightSide = false;
+                    if (this.DirectedToRightSide == false && this.X > 1)
+                        X--;
+                    if (this.X == 2) DirectedToRightSide = true;
+
+                    Console.SetCursorPosition(X, Y);
+                    Console.Write('2');
+                }
+            }
+
+            public class DestinationPoint : Object
+            {
+                public bool PlayerReachedDestinationPoint = false;
+            }
+
+            public class Walls
+            {
+                public static Boolean[,] walls = new bool[Field.YLimit, Field.XLimit];
+            }
+
+            static void Main()
+            {
+                // Инициализация параметров
+                Console.BufferWidth = Console.WindowWidth = 91;
+                Console.BufferHeight = Console.WindowHeight = 31;
+                // Прячем курсор для красоты
+                Console.CursorVisible = false;
+                // Установка параметра задержки смены кадров
+                const int frameDelay = 100;
+                var stopwatch = new Stopwatch();
+
+                // Создаём игровые объекты
+                var field = new Field();
+
+                //d4n0n - Создать стены(пример обводки)
+                for (int i = 0; i < Field.YLimit; i++)
+                    for (int j = 0; j < Field.XLimit; j++)
+                        if (i == 0 || j == 0 || i % 2 == 0 || j == Field.XLimit - 1)
+                        {
+                            Console.SetCursorPosition(j, i);
+                            Console.Write('#');
+                        }
+
+                //d4n0n - player create
+                Player player = new Player().Create();
+
+                //d4n0n - создать ghost'ов
+                var ghosts = new Ghost().CreateGhosts();
+                
+                //d4n0n - создание destination point
+                DestinationPoint point = new DestinationPoint();
+                var random = new Random();
+                point.X = random.Next(Field.XLimit - 2) + 1;
+                point.Y = Field.YLimit - 3;
+                Console.SetCursorPosition(point.X, point.Y);
+                Console.Write("%");
+
+                bool death = false;
+                bool gameOver = false;
+                //Игровой цикл
+                while (!gameOver)
+                {
+                    stopwatch.Start();
+                    // В нём нужно выполнять действия по нажатию кнопок
+                    // Обновлять положения объектов
+                    // Определять столкновения и всё такое
+
+                    // Обработка нажатий клавиатуры
+                    if (Console.KeyAvailable)
                     {
                         var keyPressed = Console.ReadKey(true).Key;
                         while (Console.KeyAvailable)
@@ -171,27 +191,42 @@ namespace ConsoleGameSolution
                         if (keyPressed == ConsoleKey.Escape) break;
 
                         // Перемещение объектов по нажатию кнопок
-                        players[i].Move(keyPressed);
+                        player.Move(keyPressed);
                     }
+
+                    // d4n0n - ДВИЖЕНИЕ ghosts
+                    for (int i=0;i<ghosts.Count;i++)
+                        ghosts[i].Move();
+
+                    //d4n0n - проверка на столкновение
+                    for (int i = 0; i < ghosts.Count - 1; i++)
+                        for (int j = 0; i < 2; i++)
+                            if (player.X == ghosts[i].X && player.Y == ghosts[i].Y)
+                            {
+                                gameOver = true;
+                                death = true;
+                            }
+
+                    //d4n0n -  проверка на достижение цели
+                    if (player.X == point.X && player.Y == point.Y)
+                        gameOver = true;
+
+
+                    // Вычисление задержки для поддержания стабильного FPS
+                    stopwatch.Stop();
+                    int sleepTime = Math.Max(frameDelay - (int)stopwatch.Elapsed.TotalMilliseconds, 0);
+                    // Задержка
+                    Thread.Sleep(sleepTime);
                 }
 
-                for (int i = 0; i < ghosts.Count; i++)
-                    for (int j=0; i < 2; i++)
-                        if (players[j].X == ghosts[i].X && players[j].Y == ghosts[i].Y)
-                            gameOver = true;
-
-                //НУЖНО РЕАЛИЗОВАТЬ ДВИЖЕНИЕ ГОСТОВ
-
-
-                // Вычисление задержки для поддержания стабильного FPS
-                stopwatch.Stop();
-                int sleepTime = Math.Max(frameDelay - (int)stopwatch.Elapsed.TotalMilliseconds, 0);
-                // Задержка
-                Thread.Sleep(sleepTime);
+                Console.Clear();
+                if (death)
+                    Console.WriteLine("You've lost.");
+                else Console.WriteLine("You win!");
+                Console.WriteLine("Escape to exit.");
+                while (Console.ReadKey(true).Key != ConsoleKey.Escape)
+                { }
             }
-            Console.Clear();
-            Console.WriteLine("You've lost.");
-            Console.ReadLine();
         }
     }
 }
